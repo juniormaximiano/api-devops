cf44f303f1c0f2d2405d6f2af946c1ff89beab3a
juniormaximiano <juniormaximianoadeilson@hotmail.com>
2026-04-13 09:56:10 -0300

    feat: cria classe principal com entry point do spring boot

---
+++ src/main/java/com/faculdade/Application.java
@@ -0,0 +1,10 @@
+package com.faculdade;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("API Rodando!");
+    }
+}
